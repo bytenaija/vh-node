251aec46de162b1f7140dff7bdf1deb53ef7f36e
HackerRank <hackerrank@example.com>
2019-02-14 05:12:27 +0000

    Saved at Thu Feb 14 2019 06:12:26 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -15,7 +15,7 @@ var addEvent = (req, res) => {
         if (event) {
             res.status(400).json({error: true, message: 'Event alread exists'});
         }else{
-            e
+            events.
         }
     })
     events.create({id: event.id, })
