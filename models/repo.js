16167a38c7ae9a0ca382d362386258674dc15f92
HackerRank <hackerrank@example.com>
2019-02-14 04:55:34 +0000

    Saved at Thu Feb 14 2019 05:55:33 GMT+0100 (West Africa Standard Time)

--- models/repo.js
+++ models/repo.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose');
 let EventSchema = mongoose.Schema({
     id: String,
     name: String,
-    url: Sting
+    url: String
 });
 
 module.exports = mongoose.model('Event', EventSchema);
