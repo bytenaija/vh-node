2dfde12faaefc6c34c6551efdab70dee4a793b84
HackerRank <hackerrank@example.com>
2019-02-14 05:20:42 +0000

    Saved at Thu Feb 14 2019 06:20:40 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -39,7 +39,7 @@ var addEvent = (req, res) => {
                    });
                    actors.find({id: actor.id}).then(actor => {
                        if (actor) {
-                           
+                           cons
                        } else {
                            
                        }
