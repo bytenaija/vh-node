ce25a518fd2462517e8908835664e206c9d9817d
HackerRank <hackerrank@example.com>
2019-02-14 05:11:26 +0000

    Saved at Thu Feb 14 2019 06:11:25 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -13,7 +13,7 @@ var addEvent = (req, res) => {
     let { repo, actor } = event;
     events.find({id: event.id}).then(event =>{
         if(event){
-            res.status(400).jso
+            res.status(400).json({})
         }
     })
     events.create({id: event.id, })
