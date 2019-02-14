4b0309de52ee040be11f02cc708f1b435c7a4a17
HackerRank <hackerrank@example.com>
2019-02-14 05:21:03 +0000

    Saved at Thu Feb 14 2019 06:21:02 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -41,7 +41,7 @@ var addEvent = (req, res) => {
                        if (actor) {
                            console.log(actor);
                        } else {
-                            repo.create(repo).then(repo => {
+                            .create(repo).then(repo => {
                                console.log(repo);
                            }).catch(e => {
                                console.log(e);
