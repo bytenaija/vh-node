89d142f66bb90372df766ef9563411980516bad9
HackerRank <hackerrank@example.com>
2019-02-14 05:30:23 +0000

    Saved at Thu Feb 14 2019 06:30:22 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -44,7 +44,7 @@ var getByActor = (req, res) => {
     .populate('repo')
     .then(events => {
         if(events){
-            res.json()
+            res.sjson()
         }else{
             
         }
