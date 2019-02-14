80227e1a40e8fe548dc60e87360c7cfcbf9df458
HackerRank <hackerrank@example.com>
2019-02-14 05:25:04 +0000

    Saved at Thu Feb 14 2019 06:25:02 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -29,7 +29,7 @@ var addEvent = (req, res) => {
                     repos.find({id: repo.id}),
                     actors.find({id: actor.id})
                     ]).then(results => {
-                        res.status(201).json()   
+                        res.status(201).json();
                     }).catch(err =>{
                         
                     });
