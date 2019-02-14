73ebfa5df01da97c1d0f45d1db33d60e40bebacb
HackerRank <hackerrank@example.com>
2019-02-14 04:36:35 +0000

    Saved at Thu Feb 14 2019 05:36:33 GMT+0100 (West Africa Standard Time)

--- models/event.js
+++ models/event.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose');
 let EventSchema = mongoose.Schema({
     id: {type: String, required: true},
     type: {type: String, required: true},
-    actor: {
+   
 })
 
 EventSchema.virtual('actor', {
