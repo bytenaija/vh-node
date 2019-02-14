30760b0b1130e406df659f7d8c10c04e264a3a05
HackerRank <hackerrank@example.com>
2019-02-14 04:55:05 +0000

    Saved at Thu Feb 14 2019 05:55:03 GMT+0100 (West Africa Standard Time)

--- models/repo.js
+++ models/repo.js
@@ -3,8 +3,7 @@ let mongoose = require('mongoose');
 let EventSchema = mongoose.Schema({
     id: String,
     type: String,
-    actor: {type: mongoose.SchemaTypes.ObjectId, ref: 'Actor'},
-    repo: {type: mongoose.SchemaTypes.ObjectId, ref: 'Repo'},
+   
 });
 
 module.exports = mongoose.model('Event', EventSchema);
