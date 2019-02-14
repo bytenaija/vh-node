a1883a669de840d6ec766dd5af91357fadb18629
HackerRank <hackerrank@example.com>
2019-02-14 04:59:31 +0000

    Saved at Thu Feb 14 2019 05:59:29 GMT+0100 (West Africa Standard Time)

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ mongoose.connect('mongodb://goatti:goattiproductionpassword1@localhost:27017/goa
         console.log(err)
         throw err
     } 
-    winston.info("Connected to MongoDB");
+    conso.info("Connected to MongoDB");
 });
 
 app.use('/', index);
