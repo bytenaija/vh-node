d6914b1da2b853634016a98bbf58bfc628c579d7
HackerRank <hackerrank@example.com>
2019-02-14 04:14:27 +0000

    Saved at Thu Feb 14 2019 05:14:25 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -1,6 +1,6 @@
 const events =  require('../models/event')
 var getAllEvents = (req, res) => {
-    events.find({}).then(events => res.status(200))
+    events.find({}).then(events => res.status(200).)
 };
 
 var addEvent = () => {
