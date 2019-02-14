076f18c1e9c3f67299da49bbae1149f3973845a0
HackerRank <hackerrank@example.com>
2019-02-14 04:57:00 +0000

    Saved at Thu Feb 14 2019 05:56:58 GMT+0100 (West Africa Standard Time)

--- models/actor.js
+++ models/actor.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose');
 let ActorSchema = mongoose.Schema({
     id: String,
     login: String,
-    url: String
+    aurl: String
 });
 
 module.exports = mongoose.model('Actor', ActorSchema);
