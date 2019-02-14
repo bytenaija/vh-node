d9d71c8bd534cfaa052fad83e3db045514de032f
HackerRank <hackerrank@example.com>
2019-02-14 05:01:07 +0000

    Saved at Thu Feb 14 2019 06:01:06 GMT+0100 (West Africa Standard Time)

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect('mongodb://goatti:goattiproductionpassword1@localhost:27017/goatti', {
+mongoose.connect('', {
     useNewUrlParser: true
 }, (err, connect) => {
     if (err) {
