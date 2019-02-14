d8799e3b1cfc774ac0996232fee0df25ce5a73dd
HackerRank <hackerrank@example.com>
2019-02-14 04:49:35 +0000

    Saved at Thu Feb 14 2019 05:49:33 GMT+0100 (West Africa Standard Time)

--- models/event.js
+++ models/event.js
@@ -3,7 +3,8 @@ let mongoose = require('mongoose');
 let EventSchema = mongoose.Schema({
     id: String,
     type: String,
-    actor: {type: mongoose.SchemaTypes.ObjectId, ref: 'Actor'
+    actor: {type: mongoose.SchemaTypes.ObjectId, ref: 'Actor'},
+    
 },
 {
     timestamps: {
