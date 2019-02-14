d05c789a8fb2959361d981c041ebf3ce5c079afb
HackerRank <hackerrank@example.com>
2019-02-14 05:13:32 +0000

    Saved at Thu Feb 14 2019 06:13:31 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -18,7 +18,7 @@ var addEvent = (req, res) => {
             events.create(
                 {
                     id: event.id, 
-                type: event.type}
+                type: event.type,}
                 )
         }
     })
