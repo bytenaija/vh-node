fc5c9df19a19dc236b43a9333dc6f25a45fe04d2
HackerRank <hackerrank@example.com>
2019-02-14 05:23:34 +0000

    Saved at Thu Feb 14 2019 06:23:33 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -26,7 +26,7 @@ var addEvent = (req, res) => {
                 }
                 ).then(event => {
                 Promise.all([
-                    repos.find({id: repo.id});
+                    repos.find({id: repo.id})
                    actors.find({id: actor.id});
                     ])
                    
