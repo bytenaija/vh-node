f4013912e5e602a277d99b454e849891b75a6985
HackerRank <hackerrank@example.com>
2019-02-14 04:56:37 +0000

    Saved at Thu Feb 14 2019 05:56:35 GMT+0100 (West Africa Standard Time)

---
+++ models/actor.js
@@ -0,0 +1,9 @@
+let mongoose = require('mongoose');
+
+let RepoSchema = mongoose.Schema({
+    id: String,
+    name: String,
+    url: String
+});
+
+module.exports = mongoose.model('Repo', RepoSchema);
--- models/repo.js
+++ models/repo.js
@@ -6,4 +6,4 @@ let RepoSchema = mongoose.Schema({
     url: String
 });
 
-module.exports = mongoose.model('Rep', RepoSchema);
+module.exports = mongoose.model('Repo', RepoSchema);
