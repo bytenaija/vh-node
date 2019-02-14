63c4af62969c6cd29ead502a28ba6f425386d73e
HackerRank <hackerrank@example.com>
2019-02-14 04:50:09 +0000

    Saved at Thu Feb 14 2019 05:50:07 GMT+0100 (West Africa Standard Time)

--- models/event.js
+++ models/event.js
@@ -4,7 +4,7 @@ let EventSchema = mongoose.Schema({
     id: String,
     type: String,
     actor: {type: mongoose.SchemaTypes.ObjectId, ref: 'Actor'},
-    role: {type: mongoose.SchemaTypes.ObjectId, ref: 'Actor'},
+    roe: {type: mongoose.SchemaTypes.ObjectId, ref: 'Actor'},
     
 },
 {
