f517f436ca0db83deebd91d0fc9891fd72f90a9f
HackerRank <hackerrank@example.com>
2019-02-14 04:18:13 +0000

    Saved at Thu Feb 14 2019 05:18:11 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -1,8 +1,8 @@
 const events =  require('../models/event')
 var getAllEvents = (req, res) => {
-    events.find({}).sort({'_id': })
+    events.find({}).sort({'_id': 'asc'})
     .then(events => res.status(200).json(events))
-    .catch(err => res.status(500).json({error: err}))
+    .catch(err => res.status(500).json({error: err}));
 };
 
 var addEvent = () => {
