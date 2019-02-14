b81aa61522e6400182680387a52ae69725761b9d
HackerRank <hackerrank@example.com>
2019-02-14 05:32:24 +0000

    Saved at Thu Feb 14 2019 06:32:15 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -50,7 +50,7 @@ var getByActor = (req, res) => {
         }
         
     }).catch(err =>{
-        res.status(500).json({success: false, messag})
+        res.status(500).json({success: false, message: 'A'})
     })
 };
 
