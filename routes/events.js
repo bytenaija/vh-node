e2bda9b4efbe320115333af4c11f63406cc913fc
HackerRank <hackerrank@example.com>
2019-02-14 05:03:55 +0000

    Saved at Thu Feb 14 2019 06:03:54 GMT+0100 (West Africa Standard Time)

--- routes/events.js
+++ routes/events.js
@@ -3,6 +3,7 @@ var router = express.Router();
 let EventsController = require('../controllers/events');
 // Routes related to event
 
-router.post('/', EventsController.add)
+router.post('/', EventsController.addE
+)
 
 module.exports = router;
