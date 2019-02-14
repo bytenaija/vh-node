abffdba93308c299eb8fac7c27384a904f7eb9fb
HackerRank <hackerrank@example.com>
2019-02-14 05:16:02 +0000

    Saved at Thu Feb 14 2019 06:16:00 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -24,7 +24,7 @@ var addEvent = (req, res) => {
                     created_at: event.created_at
                 }
                 ).then(event => {
-                   repo.find({id: i})
+                   repo.find({id: rid})
                 })
         }
     })
