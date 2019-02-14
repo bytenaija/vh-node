0df59d164a444c3eee79d95c091edc71795c5d1f
HackerRank <hackerrank@example.com>
2019-02-14 05:21:14 +0000

    Saved at Thu Feb 14 2019 06:21:12 GMT+0100 (West Africa Standard Time)

--- controllers/events.js
+++ controllers/events.js
@@ -41,7 +41,7 @@ var addEvent = (req, res) => {
                        if (actor) {
                            console.log(actor);
                        } else {
-                            actor.create(actor).then(act => {
+                            actor.create(actor).then(actor => {
                                console.log(repo);
                            }).catch(e => {
                                console.log(e);
